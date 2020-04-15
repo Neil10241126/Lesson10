b8b0fa4d43fe0ca781da5e104ce71e5f5b44c49e
LE-S02 <Neil10241126@gmail.com>
2020-04-15 21:36:30 +0800

    123

---
+++ lission02/Case06.py
@@ -0,0 +1,10 @@
+# -*- coding:UTF-8 -*-
+
+exchane = 29.123
+print('%09.4f'% exchane)  # 09為滿足9位數
+exchane = 129.123
+print('%09.4f'% exchane)
+exchane = 9.123
+print('%09.4f'% exchane)
+exchane = 8765432.123
+print('%09.4f'% exchane)
