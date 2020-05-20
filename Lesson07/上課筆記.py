dab64ce3a0b1d8a48e95a3e0901e504438239b5a
Neil <Neil10241126@gmail.com>
2020-05-20 21:35:44 +0800

    python

---
+++ Lesson07/上課筆記.py
@@ -0,0 +1,21 @@
+爬蟲相關資源
+-------------------------------------------------------------------------
+Beautiful Soup
+
+Python 使用 Beautiful Soup 抓取與解析網頁資料，開發網路爬蟲教學
+https://blog.gtwang.org/programming/python-beautiful-soup-module-scrape-web-pages-tutorial/
+
+-------------------------------------------------------------------------
+Selenium
+1.
+用 Python 匯入 Selenium 的方式，以及如何用 mac 安裝 ChromeDriver
+https://medium.com/@brettlin_78528/%E7%94%A8-python-%E5%8C%AF%E5%85%A5-selenium-%E7%9A%84%E6%96%B9%E5%BC%8F-%E4%BB%A5%E5%8F%8A%E5%A6%82%E4%BD%95%E7%94%A8mac-%E5%AE%89%E8%A3%9D-chromedriver-5d92121c02d7
+
+用 Python 匯入 Selenium 的方式，以及如何用 windows 安裝 ChromeDriver
+https://medium.com/@NorthBei/%E5%9C%A8windows%E4%B8%8A%E5%AE%89%E8%A3%9Dpython-selenium-%E7%B0%A1%E6%98%93%E6%95%99%E5%AD%B8-eade1cd2d12d
+
+2. python網路爬蟲教學-Selenium基本操作
+https://freelancerlife.info/zh/blog/python%E7%B6%B2%E8%B7%AF%E7%88%AC%E8%9F%B2%E6%95%99%E5%AD%B8-selenium%E5%9F%BA%E6%9C%AC%E6%93%8D%E4%BD%9C/
+
+3. 爬蟲_104人力銀行工作清單範例教學
+https://tlyu0419.github.io/2019/04/18/Crawl-JobList104/
